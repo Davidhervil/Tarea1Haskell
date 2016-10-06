dc13871c4ceb9ee0917e9d20189fd13b07994a5f
David <davidhervil@hotmail.com>
2016-10-06 17:08:12 -0400

    unzipF Listo

--- Tarea1.hs
+++ Tarea1.hs
@@ -1,10 +1,12 @@
 unzipR :: [(a,b)] -> ([a],[b])
 unzipR [] = ([],[])
-unzipR (p:ps) = ( fst (p):fst (unzip ps),snd (p):snd (unzip ps) )
+unzipR (p:ps) = ( fst (p):fst (unzip ps), snd (p):snd (unzip ps) )
 
---unzipF :: [(a,b)] -> ([a],[b])
---unzipF [] = ([],[])
-
+unzipF :: [(a,b)] -> ([a],[b])
+unzipF [] = ([],[])
+unzipF ps = foldr func ([],[]) ps
+	where 
+		func (a,b) (l1,l2) = (a:l1,b:l2)
 
 unzipM :: [(a,b)] -> ([a],[b])
 unzipM [] = ([],[])
