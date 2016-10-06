d7a0d29fd004c032b3c5c8ea2f5b14ab1c3de856
arturo1212 <arturoantoniotr@gmail.com>
2016-10-06 16:32:24 -0400

    fhfhfh

--- Tarea1.hs
+++ Tarea1.hs
@@ -1,5 +1,11 @@
-unzipR:: [(a,b)] -> ([a],[b])
+unzipR :: [(a,b)] -> ([a],[b])
 unzipR [] = ([],[])
 unzipR (p:ps) = ( fst (p):fst (unzip ps),snd (p):snd (unzip ps) )
 
-unzipF:: [(a,b)] -> ([a],[b])
+--unzipF :: [(a,b)] -> ([a],[b])
+--unzipF [] = ([],[])
+
+
+unzipM :: [(a,b)] -> ([a],[b])
+unzipM [] = ([],[])
+unzipM a   = (map fst a, map snd a)
