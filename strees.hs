dfb1ead3332aa26ff2bf338b6f627484cbdda0c8
Elechero <davidhervil@hotmail.com>
2016-10-09 00:55:56 -0400

    getIndices Listo

--- strees.hs
+++ strees.hs
@@ -18,3 +18,11 @@ isSubstring s1 s2 = any (isPrefix s1) (suffixes s2)
 
 findSubstrings :: String -> String -> [Int]
 findSubstrings s1 s2 = findIndices True $ map (isPrefix s1) suffixes s2
+
+--Segunda Parte
+getIndices :: SuffixTree -> [Int]
+getIndices (Leaf a) = [a]
+getIndices (Node st )= foldl irHoja [] st
+						where
+							irHoja l (_,Leaf x) = x:l
+							irHoja l (_,Node t) = foldl irHoja l t
