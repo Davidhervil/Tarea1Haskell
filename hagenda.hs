7e93c38aaa1e66671dafc7ec476c21e5c862e684
Elechero <davidhervil@hotmail.com>
2016-10-12 15:04:21 -0400

    Ajuste menor de identaciones

--- hagenda.hs
+++ hagenda.hs
@@ -36,20 +36,20 @@ pixel :: Char -> Picture
 pixel c = Picture { height=1, width=1, pixels = [[c]]}
 
 above :: Picture -> Picture -> Picture
-above p0 p1 = if width p0 == width p1 then
-										Picture { height= height p0 + width p1,
-												 width= width p0,
-												 pixels = pixels p0 ++ pixels p1 
-												}
-									else
-										error "can’t ’above’ different widths"
+above p0 p1  = if width p0 == width p1 then
+					Picture { height= height p0 + width p1,
+							 width= width p0,
+							 pixels = pixels p0 ++ pixels p1 
+							}
+				else
+					error "can’t ’above’ different widths"
 
 beside :: Picture -> Picture -> Picture
 beside p0 p1 = if height p0 == height p1 then
-											Picture { height= height p0,
-												 	  width= width p0 + width p1,
-													  pixels = beside' (pixels p0) (pixels p1)
-													}
+					Picture { height= height p0,
+						 	  width= width p0 + width p1,
+							  pixels = beside' (pixels p0) (pixels p1)
+							}
 				else 
 					error "can’t ’beside’ different height"
 				where 
