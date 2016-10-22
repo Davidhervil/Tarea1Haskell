7e4ddcb29ebf1cd129d6ccffb31eeec9621c385a
Elechero <davidhervil@hotmail.com>
2016-10-22 19:30:01 -0400

    Comments and style

--- strees.hs
+++ strees.hs
@@ -3,29 +3,30 @@ data SuffixTree = Leaf Int
 				deriving(Eq,Ord,Show)
 
 isPrefix ::  String -> String -> Bool
-isPrefix [] _ = True
-isPrefix _ [] = False
+isPrefix   []      _   = True
+isPrefix    _     []   = False
 isPrefix (p:ps) (s:ss) = p == s && (isPrefix ps ss)
 
 removePrefix :: String -> String -> String
-removePrefix p s = if isPrefix p s then drop (length p) s
+removePrefix p s =  if isPrefix p s then drop (length p) s 
 					else s
+
 suffixes :: [a] -> [[a]]
 suffixes s = scanr (:) [] s
 
 isSubstring :: String -> String -> Bool
 isSubstring s1 s2 = any (isPrefix s1) (suffixes s2)
-
+	
 findSubstrings :: String -> String -> [Int]
 findSubstrings s1 s2 = [ n | (n,x) <-zip [0..] (map (isPrefix s1) (suffixes s2)), x ] 
 
 --Segunda Parte
 getIndices :: SuffixTree -> [Int]
 getIndices (Leaf a) = [a]
-getIndices (Node st )= foldl irHoja [] st
+getIndices (Node st )= foldl obtHoja [] st
 						where
-							irHoja l (_,Leaf x) = x:l
-							irHoja l (_,Node t) = foldl irHoja l t
+							obtHoja l (_,Leaf x) = x:l
+							obtHoja l (_,Node t) = foldl obtHoja l t
 
 findSubstrings' :: String -> SuffixTree -> [Int]
 findSubstrings' s (Leaf _)  = []
@@ -49,30 +50,30 @@ insert :: (String,Int) -> SuffixTree -> SuffixTree
 insert (s,i) (Leaf _) = error "Agregando en hojas"
 insert (s,i) (Node a) = if mismoPre == [] then Node ((s,Leaf i):a)
 						else
-							if (head.show.snd) found == 'L' then
+							if (isLeaf.snd) found then 
 								Node (map replace a)
 							else
-								if resto /= [] then
+								if resto /= [] then	
 									Node (map push a)
-								else
+								else 
 									Node (map seguir a)
-						where
-							mismoPre = [ n | n <- a, fst n /= "",(head.fst) n == head s ]
-							found = head mismoPre
-							-- if mismoPre /= [] then head mismoPre
-							--		else ("",Node []) PREGUNTAR A NOVICH
-							comunes [] _ = []
-							comunes _ [] = []
-							comunes (s1:ss1) (s2:ss2) = if s1 == s2 then s1:(comunes ss1 ss2)
-														else []
-							comun = comunes s (fst found)
-							diff = removePrefix (comunes s (fst found)) s
-							resto = removePrefix (comunes s (fst found)) (fst found)
-							replace e = if found == e then (comun,Node [(diff, Leaf i), (resto,snd found)])
-										else e
-							push e = if found == e then (comun,Node[(diff,Leaf i),(resto,snd found)])
-									 else e
-							seguir e = if e == found then (fst e,insert (diff,i) (snd found))
-									   else e
+	where
+		comunes [] _ = []
+		comunes _ [] = []
+		comunes (s1:ss1) (s2:ss2) = if s1 == s2 then s1:(comunes ss1 ss2)
+									else []
+		isLeaf (_,Leaf _) = True
+		isLeaf   (_,_)    = False
+		mismoPre = [ n | n <- a, fst n /= "",(head.fst) n == head s ]
+		found = head mismoPre
+		comun = comunes s (fst found)
+		diff = removePrefix (comunes s (fst found)) s
+		resto = removePrefix (comunes s (fst found)) (fst found)
+		replace e = if found == e then (comun,Node [(resto,snd found),(diff, Leaf i)])
+					else e
+		push e = if found == e then (comun,Node[(resto,snd found),(diff,Leaf i)])
+				 else e
+		seguir e = if e == found then (fst e,insert (diff,i) (snd found))
+				   else e
 buildTree :: String -> SuffixTree
 buildTree s = foldl (flip insert) (Node []) (reverse (zip (suffixes s) [0..]))
--- unzip.hs
+++ unzip.hs
@@ -1,17 +1,21 @@
+--|'unzipR' implementación recursiva de 'unzip'
 unzipR :: [(a,b)] -> ([a],[b])
 unzipR [] = ([],[])
 unzipR (p:ps) = ( fst (p):fst (unzip ps), snd (p):snd (unzip ps) )
 
+--|'unzipF' implementación de 'unzip' utilizando 'fold'
 unzipF :: [(a,b)] -> ([a],[b])
 unzipF [] = ([],[])
 unzipF ps = foldr func ([],[]) ps
 	where 
 		func (a,b) (l1,l2) = (a:l1,b:l2)
 
+--|'unzipM' implementación de 'unzip' utilizando 'map'
 unzipM :: [(a,b)] -> ([a],[b])
 unzipM [] = ([],[])
 unzipM a   = (map fst a, map snd a)
 
+--|'unzipL' implementación de 'unzip' usando listas por comprensión
 unzipL :: [(a,b)] -> ([a],[b])
 unzipL [] = ([],[])
 unzipL ps = ([ fst p | p <-ps ],[ snd p | p <-ps ])
