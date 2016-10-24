8f14cb44830e3735e927d936dcdd5b9b8bbeae4a
Arturo Toro <arturoantoniotr@gmail.com>
2016-10-23 23:42:51 -0400

    Update strees.hs

--- strees.hs
+++ strees.hs
@@ -2,32 +2,46 @@ data SuffixTree = Leaf Int
 				| Node [(String, SuffixTree)]
 				deriving(Eq,Ord,Show)
 
+-- | 'isPrefix' recibe dos Strings s0 y s1 y retorna True si s0 es prefijo de s1, False
+-- caso contrario.
 isPrefix ::  String -> String -> Bool
 isPrefix   []      _   = True
 isPrefix    _     []   = False
 isPrefix (p:ps) (s:ss) = p == s && (isPrefix ps ss)
 
+-- | 'removePrefix' recibe dos Strings p y s y retorna un nuevo String resultante de
+-- extraer p de s.
 removePrefix :: String -> String -> String
 removePrefix p s =  if isPrefix p s then drop (length p) s 
 					else s
 
+-- | 'suffixes' recibe una lista de elementos y retorna una lista con todos los sufijos
+-- de la lista.
 suffixes :: [a] -> [[a]]
 suffixes s = scanr (:) [] s
 
+-- | 'isSubstring' recibe dos Strings s1 y s2 y retorna True si s1 es un substring de s2.
+-- False en caso contrario.
 isSubstring :: String -> String -> Bool
 isSubstring s1 s2 = any (isPrefix s1) (suffixes s2)
-	
+
+-- | 'findSubstrings' recibe dos Strings s1 y s2 y retorna una lista de enteros
+-- con la posición de todas las ocurrencias de s1 en s2.
 findSubstrings :: String -> String -> [Int]
 findSubstrings s1 s2 = [ n | (n,x) <-zip [0..] (map (isPrefix s1) (suffixes s2)), x ] 
 
 --Segunda Parte
+
+-- | 'getIndices' recibe un árbol de sufijos y devuelve todos los valores almacenados en las
+-- hojas del arbol.
 getIndices :: SuffixTree -> [Int]
 getIndices (Leaf a) = [a]
 getIndices (Node st )= foldl obtHoja [] st
 						where
 							obtHoja l (_,Leaf x) = x:l
 							obtHoja l (_,Node t) = foldl obtHoja l t
-
+-- | 'findsubstrings' recibe un String, un árbol de sufijos y retorna una lista de enteros con
+-- los indices de todas las ocurrencias del string en el árbol.
 findSubstrings' :: String -> SuffixTree -> [Int]
 findSubstrings' s (Leaf _)  = []
 findSubstrings' s (Node st) 
@@ -39,17 +53,12 @@ findSubstrings' s (Node st)
 					a = fst $ head st
 					nodo = snd $ head st
 
-bananatree = Node [("banana", Leaf 0),
-					("a", Node [("na", Node [("na", Leaf 1),
-								("", Leaf 3)]),
-								("", Leaf 5)]),
-					("na", Node [("na", Leaf 2),
-					("", Leaf 4)])]
-
+-- | 'isLeaf' recibe una tupla con un elemento y un árbol de sufijos.
 isLeaf :: (a,SuffixTree) -> Bool
 isLeaf (_,Leaf _) = True
 isLeaf (_,_)      = False
 
+-- | 'insert' permite insertar un nuevo sufijo en el arbol. 
 insert :: (String,Int) -> SuffixTree -> SuffixTree
 insert (s,i) (Leaf _) = error "Agregando en hojas"
 insert (s,i) (Node a) = if mismoPre == [] then Node ((s,Leaf i):a)
@@ -78,9 +87,12 @@ insert (s,i) (Node a) = if mismoPre == [] then Node ((s,Leaf i):a)
 		seguir e = if e == found then (fst e,insert (diff,i) (snd found))
 				   else e
 
+-- | 'buildTree' genera un árbol de sufijos a partir de un string. 
 buildTree :: String -> SuffixTree
 buildTree s = foldl (flip insert) (Node []) $ reverse $ zip (init $ suffixes s) [0..]
 
+-- | 'longestRepeatedsubstring' Recibe un árbol de sufijos y retorna una lista que contiene
+-- los subStrings repetidos más largos.
 longestRepeatedSubstring :: SuffixTree -> [String]
 longestRepeatedSubstring (Leaf _)   = []
 longestRepeatedSubstring (Node st ) = fst $ foldl look ([],[]) st
